4ac2cb98dd96e2ff0606b7bd0f344a897972da47
raihan-off <raihan.official230@gmail.com>
2023-01-13 16:00:46 +0700

    edit view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 background-color="#2F3241">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("Selamat Datang Admin Guide.in") }}
